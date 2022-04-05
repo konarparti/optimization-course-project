74cf6a361002cfc3771235dcd679cacd88fd48fc
AntonLaptop <forstudinggusev@gmail.com>
2022-04-05 11:13:15 +0300

    add main view model

---
+++ OptimizatonMethods/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using OptimizatonMethods.Models.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_MVVM_Classes;
+
+namespace OptimizatonMethods.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        #region Variables
+        private readonly IUserRepository _userRepository;
+        private readonly ITaskRepository _taskRepository;
+        private readonly IMethodRepository _methodRepository;
+        #endregion
+
+        #region Constructors
+        public MainWindowViewModel(IUserRepository user, ITaskRepository task, IMethodRepository method)
+        {
+            _userRepository = user;
+            _taskRepository = task;
+            _methodRepository = method;
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
